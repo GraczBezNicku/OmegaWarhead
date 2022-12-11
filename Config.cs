26cde28f9999f3dabe15f0b5fcc8d23ffb329748
Misaka_ZeroTwo <45165615+zhaguanyang@users.noreply.github.com>
2022-12-11 01:32:39 -0500

    v1.0

---
+++ Config.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace OmegaWarhead
+{
+    public class Config
+    {
+        public float MinutesTillOmegaWarhead { get; set; } = 25;
+
+        public string OmegaWarheadAnnouncement { get; set; } = "Omega Warhead is initiated";
+
+        public ushort OmegaWarheadAnnouncementDuration { get; set; } = 5;
+
+        public string OmegaWarheadDeathReason { get; set; } = "Killed by Omaga Warhead";
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MEC;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+using PlayerStatsSystem;
+using RemoteAdmin;
+using YamlDotNet.Core.Tokens;
+using PluginAPI.Core.Factories;
+using static PlayerStatsSystem.DamageHandlerBase;
+
+namespace OmegaWarhead
+{
+    public class Plugin
+    {
+        [PluginConfig]
+        public Config PluginConfig;
+
+        private static CoroutineHandle _coroutineHandle;
+        private static bool _omegaWarhead;
+
+        [PluginEntryPoint("OmegaWarhead", "1.0.0", "Destory the whole facility", "Misaka_ZeroTwo")]
+        void LoadPlugin()
+        {
+            PluginAPI.Events.EventManager.RegisterEvents(this);
+            FactoryManager.RegisterPlayerFactory<PlayerFactory>(this);
+        }
+
+        [PluginEvent(ServerEventType.RoundStart)]
+        void OnRoundStart()
+        {
+            _coroutineHandle = Timing.RunCoroutine(enumerator());
+        }
+
+        [PluginEvent(ServerEventType.RoundEnd)]
+        void OnRoundEnd()
+        {
+            Timing.KillCoroutines(_coroutineHandle);
+        }
+
+        [PluginEvent(ServerEventType.WarheadDetonation)]
+        void OnWarheadDetonated()
+        {
+            if (_omegaWarhead)
+            {
+                foreach (Player hub in Player.GetPlayers<Player>())
+                {
+                    hub.ReferenceHub.playerStats.KillPlayer(new CustomReasonDamageHandler(PluginConfig.OmegaWarheadDeathReason, -1f));
+                }
+            }
+        }
+
+        [PluginEvent(ServerEventType.WaitingForPlayers)]
+        void OnWaitingForPlayers()
+        {
+            _omegaWarhead = false;
+        }
+
+        IEnumerator<float> enumerator()
+        {
+            yield return Timing.WaitForSeconds(PluginConfig.MinutesTillOmegaWarhead * 60);
+            AlphaWarheadController.Singleton.InstantPrepare();
+            AlphaWarheadController.Singleton.StartDetonation(false);
+            AlphaWarheadController.Singleton.IsLocked = true;
+            _omegaWarhead = true;
+            Server.SendBroadcast(PluginConfig.OmegaWarheadAnnouncement, PluginConfig.OmegaWarheadAnnouncementDuration);
+            yield break;
+        }
+    }
+}
